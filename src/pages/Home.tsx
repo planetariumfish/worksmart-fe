1413180ca8ab925e174d0ff9ad6224ea20351cf7
Rotem S <65527552+planetariumfish@users.noreply.github.com>
2022-09-21 17:27:41 +0300

    prettify Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Center, Image, Text } from "@chakra-ui/react";
+import { Center, Image, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 import logo from "../assets/logo.png";
 import GoogleButton from "react-google-button";
@@ -8,14 +8,16 @@ type Props = {};
 const Home = (props: Props) => {
   return (
     <Center>
-      <Text mt={5}>Take your business to the next level</Text>
-      <Image src={logo} />
-      <GoogleButton
-        className="mt-5"
-        onClick={() => {
-          window.location.href = "http://localhost:8080/api/user/google";
-        }}
-      />
+      <VStack>
+        <Text mt={5}>Take your business to the next level</Text>
+        <Image src={logo} />
+        <GoogleButton
+          className="mt-5"
+          onClick={() => {
+            window.location.href = "http://localhost:8080/api/user/google";
+          }}
+        />
+      </VStack>
     </Center>
   );
 };
