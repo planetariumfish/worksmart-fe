727a20e5cd4dbd19ce86a4c236f44baa58786afb
Rotem S <65527552+planetariumfish@users.noreply.github.com>
2022-09-21 18:32:20 +0300

    add user check to Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Box } from "@chakra-ui/react";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/nav/NavBar";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 import Input from "./pages/Input";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { User } from "./contexts/user.context";
 
 function App() {
+  const { user } = useContext(User);
   return (
     <Box
       width="100vw"
@@ -19,7 +21,7 @@ function App() {
       <NavBar />
       <Box h="3.5rem" />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={user ? <Dashboard /> : <Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/input" element={<Input />} />
       </Routes>
