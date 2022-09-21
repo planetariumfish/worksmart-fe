595f1e1da25fd6d8d0c0bfcd465580acd5ae60d1
Rotem S <65527552+planetariumfish@users.noreply.github.com>
2022-09-21 17:07:20 +0300

    styleVars for forms (need work)

---
+++ src/utils/styleVars.ts
@@ -0,0 +1,23 @@
+export const pointer_before = {
+  content: "''",
+  position: "absolute",
+  right: "-1.5rem",
+  bottom: "0",
+  width: "0",
+  height: "0",
+  borderLeft: "1.5rem solid var(--chakra-colors-teal-500)",
+  borderTop: "1.5rem solid transparent",
+  borderBottom: "1.5rem solid transparent",
+};
+
+export const pointer_after = {
+  content: "''",
+  position: "absolute",
+  left: "0",
+  bottom: "0",
+  width: "0",
+  height: "0",
+  borderLeft: "1.5rem solid var(--chakra-colors-gray-100)",
+  borderTop: "1.5rem solid transparent",
+  borderBottom: "1.5rem solid transparent",
+};
