ff4a260acd484ff1e5afc0904fdc849e9e91b695
Rotem S <65527552+planetariumfish@users.noreply.github.com>
2022-09-22 12:15:25 +0300

    base star rating component

--- src/components/results/Stars.tsx
+++ src/components/results/Stars.tsx
@@ -1,9 +1,33 @@
+import { Box, Center } from "@chakra-ui/react";
 import React from "react";
+import { BsStarFill, BsStar } from "react-icons/bs";
 
-type Props = {};
+type Props = {
+  score: number;
+};
 
-const Stars = (props: Props) => {
-  return <div>Stars</div>;
+const Stars = ({ score }: Props) => {
+  const rating: React.ReactNode[] = [];
+  const makeStars = () => {
+    const percentage = Math.floor(score * 100);
+    let stars;
+    if (percentage <= 20) stars = 1;
+    else if (percentage <= 40) stars = 2;
+    else if (percentage <= 60) stars = 3;
+    else if (percentage <= 80) stars = 4;
+    else stars = 5;
+
+    for (let i = 0; i < 5; i++) {
+      rating.push(i < stars ? <BsStarFill /> : <BsStar />);
+    }
+  };
+  makeStars();
+
+  return (
+    <Center color="yellow.500" gap={1}>
+      {rating}
+    </Center>
+  );
 };
 
 export default Stars;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,50 +1,38 @@
 import React from "react";
 import Charts from "../components/results/Charts";
+import Stars from "../components/results/Stars";
 import { User } from "../contexts/user.context";
 import axios from "../utils/axiosClient";
-
 
 type Props = {};
 
 const Dashboard = (props: Props) => {
   const { user, setUser } = React.useContext(User);
   const [result, setResult] = React.useState<object>({});
-  const [star, setStar]= React.useState<number>(0);
-
-  const validateUser = async () => {
-    try {
-      const user = await axios.get("/api/user/validate");
-      if (setUser) setUser(user.data[0]);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  const [score, setScore] = React.useState<number>(0);
 
   React.useEffect(() => {
-    validateUser();
-    getDataScience()
-    console.log(star)
+    getDataScience();
+    console.log(score);
   }, []);
-
 
   const getDataScience = async () => {
     try {
       const res = await axios.get("/api/company/result");
       // console.log(res.data);
       setResult(res.data[0]);
-      setStar(res.data[1].score);
+      setScore(res.data[1].score);
     } catch (err) {
       console.log(err);
     }
-  }
-
-
+  };
 
   return (
     <div className="d-flex justify-content-center ms-5">
+      <Stars score={score} />
       <Charts result={result} />
     </div>
   );
-}
+};
 
 export default Dashboard;
