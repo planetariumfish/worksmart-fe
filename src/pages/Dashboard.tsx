9a3d5100bbb56cf51d085053764b6b8ffe24ecb7
vladiyudi <plastikwood@gmail.com>
2022-09-22 11:28:14 +0300

    graphs

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,18 +4,21 @@ import React from "react";
 import Charts from "../components/charts";
 import { User } from "../contexts/user.context";
 import axios from "../utils/axiosClient";
+import { Rating } from 'react-simple-star-rating'
+
 
 type Props = {};
 
-
-
-const Dashboard = (props: Props) => {
+function Dashboard(props: Props) {
   const { user, setUser } = React.useContext(User);
+  const [result, setResult] = React.useState<object>({});
+  const [star, setStar]= React.useState<number>(0);
 
   const validateUser = async () => {
     try {
       const user = await axios.get("/api/user/validate");
-     if (setUser) setUser(user.data[0]);
+      if (setUser)
+        setUser(user.data[0]);
     } catch (err) {
       console.log(err);
     }
@@ -23,13 +26,40 @@ const Dashboard = (props: Props) => {
 
   React.useEffect(() => {
     validateUser();
+    getDataScience()
+    console.log(star)
   }, []);
 
+
+  const getDataScience = async () => {
+    try {
+      const res = await axios.get("/api/company/result");
+      // console.log(res.data);
+      setResult(res.data[0]);
+      setStar(res.data[1].score);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+
+
   return (
-    <div className="d-flex justify-content-center ms-5">
-     <Charts/>
+    <div className="">
+      <Rating 
+      // onClick={}
+      ratingValue={star}
+      // size={50}
+      // transition
+      // allowHalfIcon
+      // showTooltip
+      // tooltipArray={}
+      // fillColorArray={}
+      />
+      <Charts result={result}/>
+   
     </div>
   );
-};
+}
 
 export default Dashboard;
